72862b8adc08489282e2d2b2611ba44d50f97f88
hailang <hailang@webuy.ai>
2022-02-17 15:52:16 +0800

    init

---
+++ src/main/java/com/example/prometheus/study/PrometheusStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.prometheus.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prometheus/study/PrometheusStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prometheus.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
